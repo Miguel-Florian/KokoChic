ca832a61a7d05d432b3f8d7aeed6b89bff1f17dd
Miguel-Florian <migflor04@gmail.com>
2022-06-23 10:52:33 +0100

    creating routers file with all routes

---
+++ Routers/router.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+	controllers "github.com/Miguel-Florian/KokoChic/Controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func path(router *gin.Engine) {
+	r := router.Group("/kokochic")
+	{
+		r.GET("/", controllers.Index())
+		r.GET("/about", controllers.About())
+		r.GET("/contact", controllers.Contact())
+	}
+}
