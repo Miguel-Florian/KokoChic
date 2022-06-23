c63c838e4290e62c045820634fbf4641931f6fa9
Miguel-Florian <migflor04@gmail.com>
2022-06-23 10:53:13 +0100

    main.go file

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	config "github.com/Miguel-Florian/KokoChic/Config"
+	routers "github.com/Miguel-Florian/KokoChic/Routers"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Welcome to Koko Chic Services")
+	fmt.Println("Starting Server ...")
+
+	config.ConnectDB() //run a database
+	r := gin.Default()
+	r.SetTrustedProxies(nil)
+
+	routers.path(r) //Iniatialize a func router
+
+	r.Run("localhost:8080")
 }
